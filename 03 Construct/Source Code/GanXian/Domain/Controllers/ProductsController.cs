d803e612ab36cf26e34347eca996dee754fafe4f
cold1986 <735255078@qq.com>
2017-12-16 14:48:21 +0800

    init

---
+++ 03 Construct/Source Code/GanXian/Domain/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Domain.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Category()
+        {
+            ViewBag.PageName = "产品列表";
+            return View();
+        }
+
+        public ActionResult Shopcart()
+        {
+            ViewBag.PageName = "购物车";
+            return View();
+        }
+    }
+}
