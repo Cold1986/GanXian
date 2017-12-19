332a032ce6de92a504a8620fc22eaf36d18c92ee
cold1986 <735255078@qq.com>
2017-12-19 21:12:58 +0800

    updated

---
+++ 03 Construct/Source Code/GanXian/Model/products2types.cs
@@ -0,0 +1,89 @@
+﻿using System;
+namespace GanXian.Model
+{
+	/// <summary>
+	/// products2types:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+	public partial class products2types
+	{
+		public products2types()
+		{}
+		#region Model
+		private int _id;
+		private int _productid;
+		private int _typeid;
+		private int? _sort;
+		private DateTime _createdate;
+		private int? _status;
+		private string _column1;
+		private string _column2;
+		/// <summary>
+		/// auto_increment
+		/// </summary>
+		public int id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int productId
+		{
+			set{ _productid=value;}
+			get{return _productid;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int typeId
+		{
+			set{ _typeid=value;}
+			get{return _typeid;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? sort
+		{
+			set{ _sort=value;}
+			get{return _sort;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime createDate
+		{
+			set{ _createdate=value;}
+			get{return _createdate;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? status
+		{
+			set{ _status=value;}
+			get{return _status;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string column1
+		{
+			set{ _column1=value;}
+			get{return _column1;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string column2
+		{
+			set{ _column2=value;}
+			get{return _column2;}
+		}
+		#endregion Model
+
+	}
+}
+
---
+++ 03 Construct/Source Code/GanXian/Model/stocks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+namespace GanXian.Model
+{
+	/// <summary>
+	/// stocks:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+	public partial class stocks
+	{
+		public stocks()
+		{}
+		#region Model
+		private int _id;
+		private int _productid;
+		private int _inventory;
+		private int? _remindnum;
+		private DateTime _createdate;
+		private int? _status;
+		private string _column1;
+		private string _column2;
+		/// <summary>
+		/// auto_increment
+		/// </summary>
+		public int id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int productId
+		{
+			set{ _productid=value;}
+			get{return _productid;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int inventory
+		{
+			set{ _inventory=value;}
+			get{return _inventory;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? remindNum
+		{
+			set{ _remindnum=value;}
+			get{return _remindnum;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime createDate
+		{
+			set{ _createdate=value;}
+			get{return _createdate;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? status
+		{
+			set{ _status=value;}
+			get{return _status;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string column1
+		{
+			set{ _column1=value;}
+			get{return _column1;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string column2
+		{
+			set{ _column2=value;}
+			get{return _column2;}
+		}
+		#endregion Model
+
+	}
+}
+
