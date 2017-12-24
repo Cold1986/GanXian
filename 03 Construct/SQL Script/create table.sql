a91aa175b5b8188f2c1fc794f030592ca93bc588
cold1986 <735255078@qq.com>
2017-12-24 18:09:14 +0800

    新增销售表 增加产品模板

--- 03 Construct/SQL Script/create table.sql
+++ 03 Construct/SQL Script/create table.sql
@@ -17,42 +17,41 @@ origin nvarchar(50) null comment '产地',
 nw nvarchar(100) null comment '净重',
 storageCondition nvarchar(200) null comment '存放条件',
 remark nvarchar(2000) null  comment '信息',
-createDate datetime null comment '插入时间',
-status int null comment '状态', -- 0无效 1有效
+createDate datetime null comment '插入时间' default now(),
+status int null comment '状态' default 1, -- 0无效 1有效 2 草稿未发布
 column1 nvarchar(100) null, -- 备用字段1
 column2 nvarchar(100) null -- 备用字段2
 )
 CREATE INDEX Products_productName ON Products(productName(50));
-
--- 首页分类表
-create table TypeList
+-- 标签表
+create table TabList
 (
-typeId int(4) NOT NULL AUTO_INCREMENT PRIMARY KEY comment '编号',
+tabId int(4) NOT NULL AUTO_INCREMENT PRIMARY KEY comment '编号',
 typeName nvarchar(50) not null comment '类别名称',
 isShow int(2) not null comment '是否需要首页推荐', -- 0否 1是
 -- showNum int(4) not null comment '显示张数',
 isBanner int(2) not null comment '是否是banner', -- banner 为轮播效果，其他平铺',
 remark nvarchar(2000) null  comment '信息',
-createDate datetime null comment '插入时间',
+createDate datetime null comment '插入时间' default now(),
 status int null comment '状态', -- 0无效 1有效
 column1 nvarchar(100) null, -- 备用字段1
 column2 nvarchar(100) null -- 备用字段2
 )
-drop table Products2Types
--- 首页分类2产品表
-create table Products2Types
+drop table Products2Tabs
+-- 首页标签2产品表
+create table Products2Tabs
 (
 id int(4) NOT NULL AUTO_INCREMENT PRIMARY KEY comment '编号',
 productId int(4) not null comment '产品编号',
-typeId int(4) not null comment '类别编号',
+tabId int(4) not null comment '标签编号',
 sort int(4) null comment '排序规则', -- 排序规则，优先时间
-createDate datetime not null comment '插入时间', 
+createDate datetime not null comment '插入时间' default now(), 
 status int null comment '状态', -- 0无效 1有效
 column1 nvarchar(100) null, -- 备用字段1
 column2 nvarchar(100) null, -- 备用字段2
 
 FOREIGN KEY (productId) REFERENCES Products (productId),
-FOREIGN KEY (typeId) REFERENCES TypeList (typeId)
+FOREIGN KEY (tabId) REFERENCES TabList (tabId)
 )
 drop table Stocks
 -- 库存表\ 是否需要提醒补货
@@ -73,7 +72,7 @@ FOREIGN KEY (productId) REFERENCES Products (productId)
 -- 用户
 create table Users
 (
-id int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY comment '编号',
+userId int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY comment '编号',
 openid nvarchar(40) null comment '微信openid',
 -- userName nvarchar(40) null comment '用户名',
 -- userPic nvarchar(100) null comment '用户头像',
@@ -89,5 +88,36 @@ column2 nvarchar(100) null, -- 备用字段2
 
 -- 用户地址
 -- 购物车
+-- 销售单表
+create table SalesSlip
+(
+salesId int(8) not  NULL AUTO_INCREMENT PRIMARY KEY comment '编号',  -- 销售单号
+userId int(8) null, -- 用户id,
+userAddress nvarchar(400), -- 用户地址
+userPhone nvarchar(20),-- 用户号码
+amount varchar(20) null, -- 总金额
+wechatSalesId varchar(40) null,-- 微信交易id
+createDate datetime not null comment '插入时间' default now(), 
+status int null comment '状态', -- 0未付款 1已付款
+column1 nvarchar(100) null, -- 备用字段1
+column2 nvarchar(100) null -- 备用字段2
+)
+
+-- 销售单产品明细表
+create table Sales2Products
+(
+id  int(8) not  NULL AUTO_INCREMENT PRIMARY KEY comment '编号',  -- 自增
+salesId int(8) not null comment '销售单号',
+productId int(4) not null comment '产品编号',
+originalPrice varchar(50)  null comment '原价',
+discountedPrice  varchar(50)  null comment '折后价',
+num int(4) not null comment '购买数量',salesslip
+salesPrice varchar(50) not null comment '实际付款价，理论上应该是 折后价*数量',
+createDate datetime not null comment '插入时间' default now(), 
+status int null comment '状态', -- 0未付款 1已付款
+column1 nvarchar(100) null, -- 备用字段1
+column2 nvarchar(100) null -- 备用字段2
+
+)
 -- 购物记录
 -- 收藏
--- 03 Construct/SQL Script/data init.sql
+++ 03 Construct/SQL Script/data init.sql
@@ -1,9 +1,28 @@
-use ganxian
+insert  INTO products(productName,specs,originalPrice,discountedPrice,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark) values('虎头鱼','9两','￥488','￥458','/img/product04.jpg','','','','舟山','1800克','8度以下','  我们叫虎头鱼，长相比较奇特，个头也不大，但是营养绝对在石浦海鱼类数一数二了，煲出来的鱼汤浓白，通常作为给小孩补身体食用。
+    抛竿串钩在渔山岛海钓的话收获颇丰，注意背上的刺，有点毒，戳到可能会肿几天，所以钓这种鱼尽量带上手套！
+')
 
-insert  INTO  products(productName,specs,originalPrice,discountedPrice,discountedExpiredDate,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark,createDate,status)
- select  '小眼带鱼77' productName ,'5两' specs,'197' originalPrice,'¥179.00' discountedPrice,NULL discountedExpiredDate,'/img/product01.jpg' pic1 ,'' pic2,''pic3,''pic4,'舟山' origin,'2000克' nw, '5度以下 ' storageCondition, '东海本地带鱼，每年入冬后小眼带鱼又肥又鲜，清蒸出油，入口即化，鲜美无比。带鱼的麟含有多种营养成分，千万不要刮掉哦。唯一的问题就是小眼带鱼因为肉质细嫩，出水后很容易破肚，这个我们也没法避免，所以介意的朋友请慎拍。往往大家也会把破肚作为衡量是否为正宗小眼带鱼的标准之一。
+insert  INTO products(productName,specs,originalPrice,discountedPrice,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark) values('产品名称','规格','原价','折后价','图片1','图片2','图片3','图片4','产地','净重','存放条件','信息')
+
+insert  INTO products(productName,specs,originalPrice,discountedPrice,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark) values('小眼带鱼','5两','￥197','￥179','/img/product01.jpg','','','','舟山','2000克','5度以下','东海本地带鱼，每年入冬后小眼带鱼又肥又鲜，清蒸出油，入口即化，鲜美无比。带鱼的麟含有多种营养成分，千万不要刮掉哦。唯一的问题就是小眼带鱼因为肉质细嫩，出水后很容易破肚，这个我们也没法避免，所以介意的朋友请慎拍。往往大家也会把破肚作为衡量是否为正宗小眼带鱼的标准之一。
 带鱼吃口很凶，所以它肚子里小鱼小虾很多，再加上长度不利于真空包装，所以我都是事先称重拍照，再进行剪嘴，清理内脏，切段真空包装，介意的朋友请提前说明。
 渔山岛海钓带鱼的最佳时节是每年的789月份，包船夜钓，包船费用4500-5000左右，需要的可事先联系客服咨询。
-【本店所售带鱼的常规规格为5-7两/条，需要其他规格的请联系客服】' remark,now(),1 ;
+【本店所售带鱼的常规规格为5-7两/条，需要其他规格的请联系客服】')
 
-truncate table products
+insert  INTO products(productName,specs,originalPrice,discountedPrice,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark) values('方头鱼','5两','￥196','￥188','/img/product02.jpg','','','','舟山','1800克','5度以下','方头鱼也是深海鱼类，色泽鲜红，头方方的，它跟大黄鱼一样，属于鲈科，肉质与鲜美程度非常接近，适合多种做法。
+不过方头鱼又好看又好吃，价格也平民，但是小朋友忌食，尤其是体型越大的方头鱼，体内汞含量极有可能超标，建议食用一斤以内的哦。同样可能汞含量超标的还有鲨鱼、箭鱼、旗鱼、枪鱼、罗非鱼、方头鱼以及鲶鱼！
+【本店所售方头鱼的常规规格为5-7两/条，需要其他规格的请联系客服】
+')
+
+insert  INTO products(productName,specs,originalPrice,discountedPrice,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark) values('海鲈鱼','6两','￥248','￥230','/img/product03.jpg','','','','舟山','1800克','5度以下','海钓常见鱼类，雪菜烧汤或者葱油都不错，价格也实惠，一般在渔山岛矶钓为主，路亚效果也不错，吃口很凶。如果是渔山岛海钓的鲈鱼建议刺身尝试一下，口感非常棒，肉质Q弹，鲜美无比！
+【本店所售海鲈鱼的常规规格为1-2斤/条，需要其他规格的请联系客服】
+')
+
+insert  INTO products(productName,specs,originalPrice,discountedPrice,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark) values('虎头鱼','9两','￥488','￥458','/img/product04.jpg','','','','舟山','1800克','8度以下','  我们叫虎头鱼，长相比较奇特，个头也不大，但是营养绝对在石浦海鱼类数一数二了，煲出来的鱼汤浓白，通常作为给小孩补身体食用。
+    抛竿串钩在渔山岛海钓的话收获颇丰，注意背上的刺，有点毒，戳到可能会肿几天，所以钓这种鱼尽量带上手套！
+')
+
+insert  INTO products(productName,specs,originalPrice,discountedPrice,pic1,pic2,pic3,pic4,origin,nw,storageCondition,remark) values('米鱼','20两','￥1888','￥1588','/img/product05.jpg','','','','舟山','2800克','5度以下','越大越贵，五斤以上的米鱼就是不是卖鱼了，是在卖鱼鳔，鱼鳔制成鱼胶后是一味昂贵的药材，价值不菲。1斤左右的小米鱼肉质鲜美，价格实惠，多种烹饪均适合，还可以做鱼丸，鱼面。做松鼠鱼是一个不错的烹饪方法')\
+
+
+-- test part
