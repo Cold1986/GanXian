40b386d67ef8e4ba81ac77e98ea84f8bbb8fa7ec
cold1986 <735255078@qq.com>
2017-12-29 09:04:46 +0800

    首页增加缓存，缓存1天

--- 03 Construct/Source Code/GanXian/Domain/Controllers/IndexController.cs
+++ 03 Construct/Source Code/GanXian/Domain/Controllers/IndexController.cs
@@ -1,6 +1,8 @@
-﻿using Domain.Models;
+﻿using CommonLib;
+using Domain.Models;
 using GanXian.BLL;
 using GanXian.Model;
+using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -12,7 +14,20 @@ namespace Domain.Controllers
         public ActionResult Index()
         {
             ViewBag.PageType = "ProductsPage";
-            List<products2Tab> products2TabList = products2TabList = ProductsBiz.CreateNew().getHomePageShowProducts2Tab();
+
+            List<products2Tab> products2TabList = new List<products2Tab>();
+            var resCache = CacheHelper.GetCache("homepageProductList");
+            if (resCache != null) products2TabList = (List<products2Tab>) resCache;
+            else {
+                products2TabList = ProductsBiz.CreateNew().getHomePageShowProducts2Tab();
+                if (products2TabList != null)
+                {
+                    var start = DateTime.Now;
+                    var expiredDate = start.AddDays(1);
+                    TimeSpan ts = expiredDate - start;
+                    CacheHelper.SetCache("homepageProductList", products2TabList, ts);
+                }
+            }
             return View(products2TabList);
         }
     }
