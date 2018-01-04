9c11978456e52a046d74993983b8b3e64f7d529d
cold1986 <735255078@qq.com>
2018-01-04 22:21:15 +0800

    添加加入购物车方法

--- 03 Construct/Source Code/GanXian/Domain/Scripts/Products/myProdDetail.js
+++ 03 Construct/Source Code/GanXian/Domain/Scripts/Products/myProdDetail.js
@@ -36,4 +36,59 @@
         autoPage: true, //自动分页
         switchLoad: "_src" //切换加载，真实图片路径为"_src"
     });
+
+    $('.btn-cart').click(function () {
+        var prodId = $("#currProdId").val();//商品Id
+        var prodCount = $("#prodCount").val();//购买数量
+        if (prodCount == 0) {
+            return;
+        }
+
+        alert(prodId);
+        alert(prodCount);
+    });
+    
 });
+
+function addShopCart() {
+    if (!allSelected) {//是否全部选中
+        return;
+    }
+    var prodId = $("#currProdId").val();//商品Id
+    var prodCount = $("#prodCount").val();//购买数量
+    alert(prodId);
+    alert(prodCount);
+    //jQuery.ajax({
+    //    url: contextPath + "/addShopBuy",
+    //    data: {
+    //        "prodId": prodId,
+    //        "count": prodCount,
+    //        "sku_id": $("#currSkuId").val(),
+    //        "distUserName": distUserName
+    //    },
+    //    type: 'post',
+    //    async: false, //默认为true 异步   
+    //    dataType: 'json',
+    //    error: function (data) {
+    //    },
+    //    success: function (retData) {
+    //        if (retData.status == 'LESS') {
+    //            floatNotify.simple(prodLessMsg);
+    //        } else if (retData.status == 'OWNER') {
+    //            floatNotify.simple(failedOwnerMsg);
+    //        } else if (retData.status == 'MAX') {
+    //            floatNotify.simple(failedBasketMaxMsg);
+    //        } else if (retData.status == 'ERR') {
+    //            floatNotify.simple(failedBasketErrorMsg);
+    //        } else if (retData.status == 'NO_SHOP') {
+    //            floatNotify.simple("商家不存在");
+    //        } else if (retData.status == 'OFFLINE') {
+    //            floatNotify.simple("该商品已经下线,不能购买！");
+    //        } else if (retData.status == "OK") {
+    //            floatNotify.simple("成功加入购物车！");
+    //            var basketCount = $("#totalNum").html();
+    //            $("#totalNum").html(Number(basketCount) + Number(prodCount));
+    //        }
+    //    }
+    //});
+}
